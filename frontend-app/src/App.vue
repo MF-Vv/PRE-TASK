f0a8b27201bfbb2ac2b505e346a8b2fd9d4fa67a
MF_Vv <112752153+MF-Vv@users.noreply.github.com>
2025-03-20 23:58:52 +0800

    Change port

--- frontend-app/src/App.vue
+++ frontend-app/src/App.vue
@@ -6,7 +6,7 @@ const message = ref('')
 
 onMounted(async () => {
   try {
-    const response = await axios.get('http://159.223.78.177') // Update with your backend URL
+    const response = await axios.get('http://159.223.78.177:5000') // Update with your backend URL
     message.value = response.data.message
   } catch (error) {
     console.error('Error fetching data:', error)
