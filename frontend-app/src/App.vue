eba04754067655d0eb1898321928bf8eeb282f1b
MF_Vv <112752153+MF-Vv@users.noreply.github.com>
2025-03-20 19:13:49 +0800

    first commit

---
+++ frontend-app/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const message = ref('')
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://localhost:5000/') // Update with your backend URL
+    message.value = response.data.message
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
+})
+</script>
+
+<template>
+  <div
+    class="min-h-screen flex flex-col items-center justify-center bg-blue-500 text-white text-2xl"
+  >
+    <h2>Hello, DevOps CI/CD</h2>
+    <p>Message: {{ message }}</p>
+  </div>
+</template>
