eeee72b978c0c376583b2e5169d413af0345c956
MF_Vv <112752153+MF-Vv@users.noreply.github.com>
2025-03-21 21:50:05 +0800

    Add

--- backend-app/app.js
+++ backend-app/app.js
@@ -7,7 +7,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from Node.js API.Hello 🚀" })
+  res.json({ message: "Hello from Node.js API.Hello New Commit Message 🚀" })
 })
 
 const PORT = process.env.PORT || 5000
